7716bbcb6182b253c45bbf8856ef40db09d1470d
startcode2021 <1372383702@qq.com>
2022-04-02 21:10:10 +0800

    加入Redis变量到期监控

---
+++ wumei-smart-master/spring-boot/ruoyi-framework/src/main/java/com/ruoyi/framework/config/RedisKeyExpirationListener.java
@@ -0,0 +1,30 @@
+package com.ruoyi.framework.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 主要作用就是:接收过期的redis消息,获取到key,key就是订单号,然后去更新订单号的状态(说明一下:用户5分钟不支付的话取消用户的订单)
+ */
+@Transactional
+@Component
+public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
+
+
+    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
+        super(listenerContainer);
+    }
+
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String orderCode = message.toString();
+
+        System.out.println("过期的订单号是: " + orderCode);
+
+    }
+}
